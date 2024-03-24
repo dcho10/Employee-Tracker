896bcdd92a6468b547a7fe2119a10a7965e56521
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-03-24 14:13:49 -0400

    Set up main prompt and questions that require an input

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer");
-
 // ```md
 // GIVEN a command-line application that accepts user input
 // WHEN I start the application
@@ -20,9 +19,94 @@ const inquirer = require("inquirer");
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
 // ```
 
-const questions = [
-    {
-        type: "input",
-        message: ""
-    }
-]
+
+// NOTES:
+
+// add department should a give a prompt (i.e. "what is the name of the department")
+
+// add role should give a prompt (i.e. "what is the name of the role"), add salary should give a prompt (i.e. what is the salary), what department does role belong to (should give response of "added to database")
+
+// add employee should ask for first name, last name, role, employee's manager
+
+
+function mainPrompt() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+            name: "management",
+        }
+    ])
+}
+
+function viewEmployees() {
+    // view all employees should return a table with id, first name, last name, title, department, salary, manager
+}
+
+function addEmployees() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter the first name.",
+            name: "firstName",
+        },
+        {
+            type: "input",
+            message:"Please enter the last name.",
+            name: "lastName",
+        },
+        {
+            type: "input",
+            message: "What is their role?",
+            name: "role",
+        },
+        {
+            type: "input",
+            message: "Who is their manager?",
+            name: "manager",
+        }
+    ])
+}
+
+function updateRole() {
+    // update employee role should let you select the name of the employee and assign them a role and give resp ("updated role")
+}
+
+function viewRole() {
+    // view all roles should return a table with id, title, department, salary
+}
+
+function addRole() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What role would you like to add?",
+            name: "role",
+        },
+        {
+            type: "input",
+            message: "What is their salary?",
+            name: "salary",
+        },
+        {
+            type: "input",
+            message: "What department do they belong to?",
+            name: "role-department"
+        },
+    ])
+}
+
+function viewDepartment() {
+    // view all departments should generate table with id and name
+
+}
+
+function addDepartment() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What role would you like to add?",
+        }
+    ])
+}
