d09bc21028978e706364f4157188e80f85ae9839
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-03-24 15:42:58 -0400

    Initialized schema and seeds.sql, set up tables for schema

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-const express = require("express");
 const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        datbase: "employees_db"
+    },
+    console.log("Connected to employess_db database.")
+);
+
